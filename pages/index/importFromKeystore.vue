25e06ed735fd77c4f5b288b587529a568f1d92e9
黄海聪 <740785956@qq.com>
2022-07-11 11:32:30 +0800

    导入钱包-Keystore导入布局完成

---
+++ pages/index/importFromKeystore.vue
@@ -0,0 +1,142 @@
+<template>
+  <view>
+    <uniHeader class="header" title="Keystore"></uniHeader>
+
+    <view class="container">
+      <view class="item">
+        <view class="item-label">Keystore导入</view>
+        <view class="item-input">
+          <u--textarea v-model="privateKey" placeholder="输入Keystore文件内容"></u--textarea>
+        </view>
+      </view>
+      <view class="item">
+        <view class="item-label">资金密码</view>
+        <view class="item-input item-input-password">
+          <u-input :password="!passwordEye" v-model="password" placeholder="输入资金密码">
+          </u-input>
+          <u-icon color="#8F9BB3" size="20" :name="passwordEye ? 'eye' : 'eye-off'" @click="passwordEye = !passwordEye">
+          </u-icon>
+        </view>
+      </view>
+      <view class="item">
+        <view class="item-label">
+          钱包名称
+        </view>
+        <view class="item-input">
+          <u-input v-model="name" placeholder="设置钱包名称"></u-input>
+        </view>
+      </view>
+    </view>
+    <u-button class="btn" @click="importWallet">导入</u-button>
+  </view>
+</template>
+
+<script>
+  import uniHeader from './components/uniHeader.vue'
+  export default {
+    components: {
+      uniHeader
+    },
+    data() {
+      return {
+        privateKey: '', // 助记词
+        password: '', // 资金密码
+        name: '', // 钱包名称
+        passwordEye: false, // 是否明文显示资金密码
+      }
+    },
+    methods: {
+      importWallet() {}
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .header {
+    border-bottom: 2rpx solid #F2F4F8;
+  }
+
+  .container {
+    padding: 0 32rpx;
+    margin-top: 48rpx;
+
+    .item {
+      margin-bottom: 48rpx;
+
+      &-label {
+        margin-bottom: 24rpx;
+        font-weight: 500;
+        font-size: 28rpx;
+        color: #2C365A;
+        line-height: 28rpx;
+      }
+
+      &-input {
+        background-color: #F2F4F8;
+        border-radius: 4rpx !important;
+
+        .u-textarea {
+          background-color: #F2F4F8;
+          height: 256rpx;
+          border-radius: 4rpx !important;
+          padding-left: 0 !important;
+
+          /deep/ textarea {
+            color: #2C365A !important;
+            font-size: 28rpx !important;
+            padding-left: 32rpx;
+            line-height: 48rpx !important;
+          }
+        }
+
+        .u-input {
+          height: 96rpx;
+          background-color: #F2F4F8;
+          border-radius: 4rpx 0 0 4rpx;
+          padding-left: 0 !important;
+
+          /deep/ input {
+            color: #2C365A !important;
+            font-size: 28rpx !important;
+            padding-left: 32rpx;
+            line-height: 48rpx !important;
+          }
+        }
+
+        /deep/ .input-placeholder {
+          // height: 48rpx !important;
+          font-weight: 400 !important;
+          font-size: 28rpx !important;
+          // color: #8397B1 !important;
+          color: #8397B1 !important;
+          // line-height: 48rpx !important;
+        }
+
+        &-password {
+          display: flex;
+
+          .u-icon {
+            height: 96rpx;
+            padding-right: 36rpx;
+            background-color: #F2F4F8;
+            border-radius: 0 4rpx 4rpx 0 !important;
+          }
+        }
+      }
+    }
+  }
+
+  .btn {
+    position: absolute;
+    bottom: 64rpx;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 622rpx;
+    height: 96rpx;
+    border-radius: 16rpx;
+    background-color: #002FA7 !important;
+    font-weight: 400;
+    font-size: 32rpx;
+    color: #FCFCFD;
+  }
+</style>
