b45c7ff56cbcebbb774235ad155d3a94ed2abdd8
张树森 <zhangss@163.com>
2022-07-19 09:30:56 +0800

    委托初始化

--- pages/delegate/index.vue
+++ pages/delegate/index.vue
@@ -1,12 +1,70 @@
 <template>
-	<view class="">
-		委托
-		 <TabBar :current-page="2" />
+	<view class="delegate">
+		<view class="container">
+			<div class="top">
+					<view class="left">
+						我的委托
+						<view class="line"/>
+					</view>
+					<view class="right">
+						验证人
+						<view class="line"/>
+					</view>
+			</div>
+			<My/>
+		</view>
+		<tab-bar :current-page="2" />
 	</view>
 </template>
 
 <script>
+import My from './components/my.vue'
+export default {
+  components: {
+    My
+  },
+  data() {
+    return {
+      list: [{
+        name: '我的委托',
+      }, {
+        name: '验证人',
+      }, ]
+    }
+  }
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.delegate {
+		.container {
+			.top {
+				display: flex;
+				height: 104rpx;
+				padding: 26rpx 32rpx;
+				font-size: 40rpx;
+				color: #2C365A;
+				border-radius: 2px;
+			}
+
+			.left {
+				width: 160rpx;
+				text-align: center;
+
+			}
+
+			.right {
+				width: 120rpx;
+				margin-left: 32rpx;
+			}
+
+			.line {
+				height: 4rpx;
+				margin-top: 12rpx;
+				background: #275EF1;
+				border-radius: 32px;
+
+			}
+		}
+	}
 </style>
